80371275c1f0e908722c0166aa097077196f8203
yumeandra <aleksandra.goerlich@gmail.com>
2016-10-14 23:55:34 +0200

    day_17 kimono-project-page_draft

--- inc_header.php
+++ inc_header.php
@@ -4,7 +4,7 @@
         <nav class="col-6">
             <ul class="menu">
                 <li class="col-1"><a href="index-building-space.php">ARTIST</a></li>
-                <li class="col-1"><a href="index-building-space.php">KI-MONO PROJECT</a></li>
+                <li class="col-1"><a href="index-kimono-project-page.php">KI-MONO PROJECT</a></li>
                 <li class="col-1"><a href="index-catalogue-page.php">CATALOGUE</a></li>
                 <li class="col-1"><a href="index-exhibitions-page.php">EXHIBITIONS</a></li>
                 <li class="col-1"><a href="index-building-space.php">PERFORMANCES</a></li>
---
+++ index-kimono-project-page.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <title>Kimono Catalogue</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="fonts/font-awesome-4.6.3/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Antic" rel="stylesheet">
+    <meta name="theme-color" content="#000"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="./images/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./images/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./images/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./images/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./images/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./images/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./images/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./images/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./images/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./images/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="./images/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./images/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="./fonts/LogoTypeGothicCondense/stylesheet.css">
+    <link rel="stylesheet" href="./node_modules/animate.css/animate.min.css">
+</head>
+<body class="animated slideInRight">
+    
+    <?php include("inc_header.php"); ?>
+    
+    <section class="container story">
+        <div class="project-story">
+            <p>
+            "Kimono …after studying Rei Kawakubo design approach (during studies of Innovative Patter Cutting at Central Saint Martins) i went on to imply my perception of how and Kawakubo is thinking  in her design.. I created the KIMONO RECONSTRUCTION Project its main purpose was to redesigns Kimono in the same way Kawakubo redesigned western garments. Knowing nothing about kimono or Japan i went on to learn and develop while following that road, what  was initially to be one year project lasted for 5 years, granting me the nonparallel opportunity to dive deep in to the Japanese culture and maybe even a soul and to visit this incredible country. Falling in love an ending up ..still not knowing how to redesign a kimono…"
+            </p>
+            <p>
+            KIMONO UN-PERFECT is an interactive installation of works by Joanna Bodzek, a Polish born artist and designer living and working in Sweden. KIMONO UN-PERFECT became a culmination of all Bodzek’s work created for <a href="http://kimonoreconstruction.joannabodzek.com/?cat=5" target="_blank">Kimono Reconstruction (2009-15)</a>, shown in its entirety.  Bodzek places this work at the intersection of both figurative and conceptual art, using the language of her own European culture to facilitate her examination of the kimono via <a href="index-catalogue-page-04.php" target="_blank">social perspective</a>, <a href="index-catalogue-page-09.php" target="_blank">fairy tales</a> and <a href="index-catalogue-page-02.php" target="_blank">works of art</a>. Kimono-like garments, video work and photography are used to look at the symbolism of this traditional Japanese dress, which is nowadays deemed difficult to interpret, even for those living within the culture from which it came. 
+            <br>
+            Bodzek’s kimonos from her Kimono Reconstruction Project, are often made from recycled materials like tea towels, <a href="index-catalogue-page-04.php" target="_blank">tablecloths</a> and other <a href="index-catalogue-page-07.php" target="_blank">handmade textiles</a> which carry their own narratives corresponding to the stereotyped female role. Her fascination with Japanese culture began with studying the work of Rei Kawakubo during her time at Central Saint Martins, and deepened later while collaborating with the Butoh artists and performers.
+            <br>
+            Artist's statement:
+            <br>
+            “Within the Kimono Reconstruction, I explore the border between both the familiar and unfamiliar in relation to tradition and ideals of beauty; things we do not recognise through the themes of identity and diversity. I am linking the cultures of West and East through this work, exploring my obsession and fascination with the mystery of the Geisha, specifically her white makeup and her ultimately mesmerising beauty. 
+            <br>
+            While considering the name of the exhibition I came across Shakespeare Sonnet 23, conveying perfectly the many layers of this work.
+            <br>
+            As an unperfect actor on the stage,<br>
+            Who with his fear is put besides his part,<br>
+
+            Or some fierce thing replete with too much rage,<br>
+            Whose strength’s abundance weakens his own heart."<br>
+            <span class="JoBo-signature">Joanna Bodzek</span> and <span class="AlGoe-signature">Aleksandra Görlich</span><br>
+            </p>
+        </div>
+    </section>
+    
+    
+     <?php include("inc_footer.php"); ?>
+    
+</body>
+</html>
