f2a4dfe6a4bd3aa577f319b8e3d09f358a036091
yumeandra <aleksandra.goerlich@gmail.com>
2016-11-14 13:32:10 +0100

    day_20 kimono-project-page

--- index-kimono-project-page.php
+++ index-kimono-project-page.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
+
 <head>
     <meta charset="UTF-8">
     <title>Kimono Catalogue</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="stylesheet" href="./css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="./css/style.css" />
     <link rel="stylesheet" href="fonts/font-awesome-4.6.3/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Antic" rel="stylesheet">
-    <meta name="theme-color" content="#000"/>
+<!--    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">-->
+<!--    <link href="https://fonts.googleapis.com/css?family=Oregano" rel="stylesheet">-->
+    <link href="https://fonts.googleapis.com/css?family=Delius" rel="stylesheet">
+    <meta name="theme-color" content="#000" />
     <link rel="apple-touch-icon" sizes="57x57" href="./images/favicons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="./images/favicons/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="./images/favicons/apple-icon-72x72.png">
@@ -17,7 +21,7 @@
     <link rel="apple-touch-icon" sizes="144x144" href="./images/favicons/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="./images/favicons/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="./images/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="./images/favicons/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="./images/favicons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./images/favicons/favicon-16x16.png">
@@ -28,59 +32,74 @@
     <link rel="stylesheet" href="./fonts/LogoTypeGothicCondense/stylesheet.css">
     <link rel="stylesheet" href="./node_modules/animate.css/animate.min.css">
 </head>
+
 <body class="animated slideInRight">
-    
+
     <?php include("inc_header.php"); ?>
-    
-    <section class="container story">
-        <div class="whole-story">
-            <div class="project-story">
-            "Kimono …after studying Rei Kawakubo design approach (during studies of Innovative Patter Cutting at Central Saint Martins) I went on to imply my perception of how and Kawakubo is thinking  in her design.. <br> I created the KIMONO RECONSTRUCTION Project. Its main purpose was to redesign Kimono in the same way Kawakubo redesigned western garments.<br><br> Knowing nothing about kimono or Japan I went on to learn and develop while following that road, what  was initially to be one year project lasted for 5 years, granting me the nonparallel opportunity to dive deep in to the Japanese culture and maybe even a soul and to visit this incredible country. Falling in love an ending up ..still not knowing how to redesign a kimono…"
-            </div>
-            <br>
-            <div class="project-story">
-            Ki-mono means a thing to wear, to carry on yourself... What do you carry on? What do you carry with you? Between rooms, empty space, a new room beyond East and West, kitchen reflections, ideals and femininity ... a space for both the cultures to meet.
-                <br><br>
-            KI-MONO becomes here a canvas on which to paint freely and unrestricted, to combine western and eastern cultural aspects, visual traditions, roots, preferences and ideals of beauty.
-            It started as an attempt, as an artistic challenge to approach, in order to re-design something so Japanese and untouched as a kimono-garment, Westernism to Japonism. But this work soon shown to be a journey…
-                <br><br>
-            Shortly it became an exploration of the influence of tradition on women's role and position in society. The Ki-mono Project becomes a voice in a cross-cultural discussion on ideals of beauty and femininity ... a space for both the cultures to meet. Through the layers of fabrics one can see the past that becomes part of the future. Like the many layers of kimono, one needs to open one after another to see the image, to see the kimono, to see the woman. The original image, roots, the origins, story, and the Time. 
-            </div>
-            <br>
-            <div class="project-story">
-            KIMONO UN-PERFECT is an interactive installation of works by Joanna Bodzek, a Polish born artist and designer living and working in Sweden. KIMONO UN-PERFECT became a culmination of all Bodzek’s work created for 
-                <a href="http://kimonoreconstruction.joannabodzek.com/?cat=5" target="_blank">Kimono Reconstruction (2009-15)</a>
-            , shown in its entirety.  Bodzek places this work at the intersection of both figurative and conceptual art, using the language of her own European culture to facilitate her examination of the kimono via 
-                <a href="index-catalogue-page-04.php" target="_blank">social perspective</a>
-            , 
-                <a href="index-catalogue-page-09.php" target="_blank">fairy tales</a> 
-            and 
-                <a href="index-catalogue-page-02.php" target="_blank">works of art</a>
-            . Kimono-like garments, video work and photography are used to look at the symbolism of this traditional Japanese dress, which is nowadays deemed difficult to interpret, even for those living within the culture from which it came. 
-                <br><br>
-            Bodzek’s kimonos from her Kimono Reconstruction Project are often made from recycled materials like tea towels, 
-                <a href="index-catalogue-page-04.php" target="_blank">tablecloths</a> 
-            and other 
-                <a href="index-catalogue-page-07.php" target="_blank">handmade textiles</a> 
-            which carry their own narratives corresponding to the stereotyped female role. Her fascination with Japanese culture began with studying the work of Rei Kawakubo during her time at Central Saint Martins, and deepened later while collaborating with the Butoh artists and performers.
-            </div>
-            <br><br>
-            <div class="project-story">
-            Artist's statement:
-            </div>
+
+        <section class="container">
+            
             <br>
             <div class="quote">
-                “Within the Kimono Reconstruction, I explore the border between both the familiar and unfamiliar in relation to tradition and ideals of beauty; things we do not recognise through the themes of identity and diversity. I am linking the cultures of West and East through this work, exploring my obsession and fascination with the mystery of the Geisha, specifically her white makeup and her ultimately mesmerising beauty. 
+                <p>
+                    “Within the Kimono Reconstruction, I explore the border between both the familiar and unfamiliar in relation to tradition and ideals of beauty; things we do not recognise through the themes of identity and diversity. I am linking the cultures of West and East through this work, exploring my obsession and fascination with the mystery of the Geisha, specifically her white makeup and her ultimately mesmerising beauty."
+                </p>
             </div>
-                <br><br>
-            
-                <span class="JoBo-signature">Joanna Bodzek</span> and <span class="AlGoe-signature">Aleksandra Görlich</span><br>
-          
-        </div>
-    </section>
-    
-    
-     <?php include("inc_footer.php"); ?>
-    
+            <br>
+            <br>
+            <div class="story">
+                <div id="illustrations" class="col-3">
+                    <div class="ill-1"></div>
+                    <div class="ill-2"></div>
+                    <div class="ill-3"></div>
+                    <div class="ill-4"></div>
+
+                </div>
+                <div class="whole-story">
+                    <div class="project-story">
+                        "Kimono …after studying Rei Kawakubo design approach (during studies of Innovative Patter Cutting at Central Saint Martins) I went on to imply my perception of how and Kawakubo is thinking in her design..
+                        <br> I created the KIMONO RECONSTRUCTION Project. Its main purpose was to redesign Kimono in the same way Kawakubo redesigned western garments.
+                        <br>
+                        <br> Knowing nothing about kimono or Japan I went on to learn and develop while following that road, what was initially to be one year project lasted for 5 years, granting me the nonparallel opportunity to dive deep in to the Japanese culture and maybe even a soul and to visit this incredible country. Falling in love an ending up ..still not knowing how to redesign a kimono…"
+                    </div>
+                    <br>
+                    <div class="project-story">
+                        Ki-mono means a thing to wear, to carry on yourself... What do you carry on? What do you carry with you? Between rooms, empty space, a new room beyond East and West, kitchen reflections, ideals and femininity ... a space for both the cultures to meet.
+                        <br>
+                        <br> KI-MONO becomes here a canvas on which to paint freely and unrestricted, to combine western and eastern cultural aspects, visual traditions, roots, preferences and ideals of beauty. It started as an attempt, as an artistic challenge to approach, in order to re-design something so Japanese and untouched as a kimono-garment, Westernism to Japonism. But this work soon shown to be a journey…
+                        <br>
+                        <br> Shortly it became an exploration of the influence of tradition on women's role and position in society. The Ki-mono Project becomes a voice in a cross-cultural discussion on ideals of beauty and femininity ... a space for both the cultures to meet. Through the layers of fabrics one can see the past that becomes part of the future. Like the many layers of kimono, one needs to open one after another to see the image, to see the kimono, to see the woman. The original image, roots, the origins, story, and the Time.
+                    </div>
+                    <br>
+                    <div class="project-story">
+                        KIMONO UN-PERFECT is an interactive installation of works by Joanna Bodzek, a Polish born artist and designer living and working in Sweden. KIMONO UN-PERFECT became a culmination of all Bodzek’s work created for
+                        <a href="http://kimonoreconstruction.joannabodzek.com/?cat=5" target="_blank">Kimono Reconstruction (2009-15)</a> , shown in its entirety. Bodzek places this work at the intersection of both figurative and conceptual art, using the language of her own European culture to facilitate her examination of the kimono via
+                        <a href="index-catalogue-page-04.php" target="_blank">social perspective</a> ,
+                        <a href="index-catalogue-page-09.php" target="_blank">fairy tales</a> and
+                        <a href="index-catalogue-page-02.php" target="_blank">works of art</a> . Kimono-like garments, video work and photography are used to look at the symbolism of this traditional Japanese dress, which is nowadays deemed difficult to interpret, even for those living within the culture from which it came.
+                        <br>
+                        <br> Bodzek’s kimonos from her Kimono Reconstruction Project are often made from recycled materials like tea towels,
+                        <a href="index-catalogue-page-04.php" target="_blank">tablecloths</a> and other
+                        <a href="index-catalogue-page-07.php" target="_blank">handmade textiles</a> which carry their own narratives corresponding to the stereotyped female role. Her fascination with Japanese culture began with studying the work of Rei Kawakubo during her time at Central Saint Martins, and deepened later while collaborating with the Butoh artists and performers.
+                    </div>
+                    <br>
+                    <br>
+
+
+                    <span class="JoBo-signature">Joanna Bodzek</span>  
+                    <br>
+                    <span class="AlGoe-signature">Aleksandra Görlich</span>
+                    <br>
+                    <br>
+
+                </div>
+            </div>
+
+        </section>
+
+
+        <?php include("inc_footer.php"); ?>
+
 </body>
+
 </html>
